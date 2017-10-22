9231a82126143c93e810ad9b86e5ace6b193f133
adalbertofjr <adalberto.fernandesjr@gmail.com>
2017-10-22 21:04:37 -0200

    Wear: Comunicacao e envio de dados

---
+++ android/WearApp/mobile/src/main/java/br/com/adalbertofjr/wearapp/WearService.java
@@ -0,0 +1,25 @@
+package br.com.adalbertofjr.wearapp;
+
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class WearService extends WearableListenerService {
+
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEventBuffer) {
+        for (DataEvent dataEvent : dataEventBuffer) {
+            if (dataEvent.getType() == DataEvent.TYPE_CHANGED) {
+                DataMap dataMap = DataMapItem.fromDataItem(dataEvent.getDataItem()).getDataMap();
+                String path = dataEvent.getDataItem().getUri().getPath();
+                if (path.equals("/step-counter")) {
+                    int steps = dataMap.getInt("step-count");
+                    long time = dataMap.getLong("timestamp");
+                }
+            }
+        }
+    }
+}
