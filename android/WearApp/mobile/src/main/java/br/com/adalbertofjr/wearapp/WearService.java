53af64b193b0b421fe3f3178c82e93f1339fda56
adalbertofjr <adalberto.fernandesjr@gmail.com>
2017-10-28 20:46:45 -0200

    Novos exemplos

--- android/WearApp/mobile/src/main/java/br/com/adalbertofjr/wearapp/WearService.java
+++ android/WearApp/mobile/src/main/java/br/com/adalbertofjr/wearapp/WearService.java
@@ -4,6 +4,7 @@ import com.google.android.gms.wearable.DataEvent;
 import com.google.android.gms.wearable.DataEventBuffer;
 import com.google.android.gms.wearable.DataMap;
 import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.WearableListenerService;
 
 public class WearService extends WearableListenerService {
@@ -20,6 +21,13 @@ public class WearService extends WearableListenerService {
                     long time = dataMap.getLong("timestamp");
                 }
             }
+        }
+    }
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        if (messageEvent.getPath().equals("/path/message")) {
+            // Do something with byte array from messageEvent.getData()
         }
     }
 }
--- android/WearApp/wear/src/main/java/br/com/adalbertofjr/wearapp/MainActivity.java
+++ android/WearApp/wear/src/main/java/br/com/adalbertofjr/wearapp/MainActivity.java
@@ -1,6 +1,7 @@
 package br.com.adalbertofjr.wearapp;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,10 +11,16 @@ import android.widget.TextView;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.Asset;
 import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
 import com.google.android.gms.wearable.PutDataMapRequest;
 import com.google.android.gms.wearable.PutDataRequest;
 import com.google.android.gms.wearable.Wearable;
+
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
@@ -57,23 +64,62 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
 
     }
 
+
+
+    // Garantia de entrega
+    // Tamanho máximo de 100kb, sincroniza com aparelho
     public void sendStepCount(int steps, long timestamp) {
         PutDataMapRequest putDataMapRequest = PutDataMapRequest.create("/step-counter");
 
         putDataMapRequest.getDataMap().putInt("step-count", steps);
         putDataMapRequest.getDataMap().putLong("timestamp", timestamp);
 
+        Bitmap bitmap = null;
+        Asset asset = createAssetFromBitmap(bitmap);
+        putDataMapRequest.getDataMap().putAsset("profileImage", asset);
+
         PutDataRequest request = putDataMapRequest.asPutDataRequest();
         Wearable.DataApi.putDataItem(mGoogleApiClient, request)
                 .setResultCallback(new ResultCallback<DataApi.DataItemResult>() {
                     @Override
                     public void onResult(@NonNull DataApi.DataItemResult dataItemResult) {
-                        if(!dataItemResult.getStatus().isSuccess()){
+                        if (!dataItemResult.getStatus().isSuccess()) {
                             // Falha ao enviar step count
-                        }else{
+                        } else {
                             // Enviado com sucesso
                         }
                     }
                 });
+    }
+
+    // Não possui garantia de entrega, vc é o responsável por tratar
+    // Mais rápido
+    // Ver Myservice para recuperar a mensagem
+    public void sendMessage() {
+        String nodeId = null;
+
+        NodeApi.GetConnectedNodesResult nodes = Wearable.NodeApi
+                .getConnectedNodes(mGoogleApiClient).await();
+
+        for (Node node : nodes.getNodes()) {
+            // Recuperar nodeID
+            nodeId = node.getId();
+        }
+
+        Wearable.MessageApi.sendMessage(
+                mGoogleApiClient, nodeId, "/path/message",
+                new byte[0]).setResultCallback(new ResultCallback<MessageApi.SendMessageResult>() {
+            @Override
+            public void onResult(@NonNull MessageApi.SendMessageResult sendMessageResult) {
+
+            }
+        });
+    }
+
+    private Asset createAssetFromBitmap(Bitmap bitmap) {
+        final ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteStream);
+
+        return Asset.createFromBytes(byteStream.toByteArray());
     }
 }
