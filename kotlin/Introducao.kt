f1acbd4bb26679b85556fb2d0d06df6709b3810f
adalbertofjr <adalberto.fernandesjr@gmail.com>
2017-11-02 23:00:16 -0200

    Learning Kotlin

---
+++ kotlin/Introducao.kt
@@ -0,0 +1,17 @@
+var age = 35
+
+var age: Int = 35
+
+// Imutável, como final no java ou let no swift
+// No Kotlin dar preferência ao uso de val ao invés de usar var
+// val > var
+val name = "Adalberto Fernandes" 
+val name:String = "Adalberto Fernandes Junior"
+
+
+// Tipos
+    // Int, Float, Double, Boolean, Long, Short, Byte
+    // Usar Float ao invés de Double
+val pontos:Int = 999
+
+// Tipos Imutáveis
